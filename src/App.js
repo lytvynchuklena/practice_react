c1777856d60d8650704487e254b4e9d2ad8848d5
lytvynchuklena <lena.melanchina@gmail.com>
2021-05-14 19:30:14 +0300

    forms_home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import './App.css';
+import {useState} from "react";
 
 
 function App() {
+
+	// let [login, setLogin] = useState('default');
+	// let [password, setPassword] = useState('');
+	//
+	// function onLoginChange(e) {
+	// 	setLogin(e.target.value);
+	// }
+	//
+	// function onPasswordChange(e) {
+	// 	setPassword(e.target.value);
+	// }
+
+	let [formState, setFormState] = useState({login: '', password: ''});
+
+
+
+	function onSubmit(e) {
+		console.log(e.target.login.value);
+		console.log(e.target.password.value);
+		e.preventDefault();
+		console.log(formState);
+	}
+
+	function onChange(e) {
+		setFormState({...formState, [e.target.name]:e.target.value})
+	}
+
+
+
 	return (
 		<div>
-
+			<form onSubmit={onSubmit}>
+				<input type="text" name={'login'} value={formState.login} onChange={onChange}/>
+				<input type="text" name={'password'} value={formState.password} onChange={onChange}/>
+				<button>Submit</button>
+			</form>
 		</div>
 	);
 }
