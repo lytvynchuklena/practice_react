dd4f7095ca057fdbd48e99cdcd120d0af71d8a0c
lytvynchuklena <lena.melanchina@gmail.com>
2021-05-16 10:17:41 +0300

    forms_home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import {createRef} from "react";
 
 
 function App() {
+
+	let login = createRef();
+	let password = createRef();
+
+	function onSubmit(e) {
+		console.log(login.current.value);
+		console.log(password.current.value);
+
+		//create obj && send to api
+		e.preventDefault();
+	}
+
 	return (
 		<div>
-
+			<form onSubmit={onSubmit}>
+				<input type="text" name={'login'} ref={login}/>
+				<input type="text" name={'password'} ref={password}/>
+				<button>log in</button>
+			</form>
 		</div>
 	);
 }
