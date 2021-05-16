39a5f4a7882452dbe0a7a6b0865892eb5f281350
lytvynchuklena <lena.melanchina@gmail.com>
2021-05-16 13:09:08 +0300

    forms_home_lesson3 axios dataService

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 import Users from "./components/users/Users";
-
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from "react-router-dom";
 
 function App() {
 	return (
 		<div>
-			<Users/>
+			{/*<Users/>*/}
+
+			<Router>
+				<Link to={'/users'}>to users</Link>
+				<br/>
+				<Link to={'/posts'}>to posts</Link>
+				<Switch>
+					<Route path={'/users'} render={()=> (<Users/>)}/>
+					<Route path={'/posts'} render={()=> (<div>posts should be here</div>)}/>
+
+				</Switch>
+			</Router>
 		</div>
 	);
 }
